649fed955af7de90a75b5b6a97ed004eba1d31bd
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-06-07 23:42:00 +0300

    adding register and repo serves

---
+++ app/Http/Controllers/API/Auth/UserAuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Services\OtpService;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Classes\ApiResponseClass;
+use App\Http\Controllers\Controller;
+use App\Repositories\UserRepository;
+
+class UserAuthController extends Controller
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct(private UserRepository $UserRepository,private OtpService $otpService)
+    {
+        //
+    }
+    public function register(Request $request){
+        $fields=$request->validate([
+            'name'=>['required','string','max:100'],
+            'phone'=>['required','string','min:9','max:15',Rule::unique('users')],
+            'password' => ['required','string','min:6','confirmed',],
+        ]);
+        // Store the new user using the UserRepository
+        $fields['user_type']='user';
+        $user=$this->UserRepository->store($fields);
+
+        // Generate a random OTP and prepare it for sending
+        $otp=$this->otpService->generateOTP($user->phone,'account_creation');
+
+        // Send an email with the OTP code to the user's email address
+        // SendOtpEmailJob::dispatch($user->email, $otp);
+        // Mail::to($user->email)->send(new OtpMail($otp));
+
+        return ApiResponseClass::sendResponse($user,'تم إرسال رمز التحقق الى رقم الهاتف :'. $user->phone);
+    }
+   
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Auth\UserAuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register',[UserAuthController::class,'register']);
