b9bd547e9db5dca7a3db572f088568577bb6be55
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-06-16 22:31:08 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // $middleware->validateCsrfTokens(except: [
-        //     '/github/deploy' // <-- exclude this route
-        // ]);
+        $middleware->validateCsrfTokens(except: [
+            '/github/deploy' // <-- exclude this route
+        ]);
         $middleware->alias([
             'check.banned' => CheckBannedUser::class,
         ]);
