3b20f72ba5cf87797c14a5fae1ee1856baa188be
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-06-21 15:21:08 +0300

    add bank

---
+++ app/Filament/Resources/BankResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BankResource\Pages;
+use App\Filament\Resources\BankResource\RelationManagers;
+use App\Models\Bank;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BankResource extends Resource
+{
+    protected static ?string $model = Bank::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
+
+    protected static ?string $navigationLabel = 'البنوك';
+    protected static ?string $pluralModelLabel = 'البنوك';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(100)
+                    ->unique(ignoreRecord: true),
+
+                Forms\Components\FileUpload::make('logo_url')
+                    ->label('Logo')
+                    ->image()
+                    ->directory('banks/logos')
+                    ->getUploadedFileNameForStorageUsing(
+                        fn (TemporaryUploadedFile $file): string => 
+                            (string) str($file->getClientOriginalName())
+                                ->prepend('bank-logo-'),
+                    )
+                    ->downloadable()
+                    ->openable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('logo_url')
+                    ->label('اللوقو')
+                    ->disk('public')
+                    ->url(fn ($record) => asset(':8000/storage/' . $record->logo_url)),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable()
+                    ->label('أسم البنك'),
+                    
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->label('أنشى في'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBanks::route('/'),
+            // 'create' => Pages\CreateBank::route('/create'),
+            // 'edit' => Pages\EditBank::route('/{record}/edit'),
+        ];
+    }
+}
