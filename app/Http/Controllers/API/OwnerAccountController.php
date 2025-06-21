a507a796a3fca27a0aa19e20b9b39ce6b2f4b339
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-06-21 20:44:53 +0300

    adding EstablishmentType model and controller

--- app/Http/Controllers/API/OwnerAccountController.php
+++ app/Http/Controllers/API/OwnerAccountController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Classes\ApiResponseClass;
+use App\Http\Controllers\Controller;
+use App\Repositories\OwnerAccountRepository;
 
 class OwnerAccountController extends Controller
 {
+     /**
+     * Create a new class instance.
+     */
+    public function __construct(private OwnerAccountRepository $OwnerAccountRepository)
+    {
+        //
+    }
+    
     /**
      * Display a listing of the resource.
      */
@@ -20,7 +32,17 @@ class OwnerAccountController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $fields=$request->validate([
+            'bank_id'=>['required',Rule::exists('banks','id')],
+            'account_number'=>['required','string','min:7','max:15'],
+        ]);
+        try {
+            $fields['owner_id'] = auth('sanctum')->id();
+            $account = $this->OwnerAccountRepository->store($fields);
+            return ApiResponseClass::sendResponse($account, 'account saved successfully.');
+        } catch (Exception $e) {
+            return ApiResponseClass::sendError('Error save account: ' . $e->getMessage());
+        }
     }
 
     /**
@@ -36,7 +58,19 @@ class OwnerAccountController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        if($id != auth('sanctum')->id()){
+            return ApiResponseClass::sendError('You are not authorized to update this acount.', [], 403);
+        }
+        $fields=$request->validate([
+            'bank_id'=>['sometimes',Rule::exists('banks','id')],
+            'account_number'=>['sometimes','string','min:7','max:15'],
+        ]);
+        try {
+            $account = $this->OwnerAccountRepository->update($fields,$id);
+            return ApiResponseClass::sendResponse($account, 'account update successfully.');
+        } catch (Exception $e) {
+            return ApiResponseClass::sendError('Error update account: ' . $e->getMessage());
+        }
     }
 
     /**
@@ -44,6 +78,17 @@ class OwnerAccountController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        if($id != auth('sanctum')->id()){
+            return ApiResponseClass::sendError('You are not authorized to deleting this acount.', [], 403);
+        }
+        try {
+            $account=$this->OwnerAccountRepository->getById($id);
+            if($this->OwnerAccountRepository->delete($id)){
+                return ApiResponseClass::sendResponse($account, "{$account->id} unsaved successfully.");
+            }
+             return ApiResponseClass::sendError("Acount with ID {$id} may not be found or not deleted. Try again.");
+        } catch (Exception $e) {
+            return ApiResponseClass::sendError('Error deleting acount: ' . $e->getMessage());
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\BankController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\Auth\OtpController;
+use App\Http\Controllers\API\OwnerAccountController;
 use App\Http\Controllers\API\Auth\UserAuthController;
+use App\Http\Controllers\API\EstablishmentTypeController;
 use App\Http\Controllers\API\Auth\ForgetPasswordController;
-use App\Http\Controllers\API\BankController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,7 +17,7 @@ Route::get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum','check.banned'])->group(function () {
     Route::put('/user/{id}', [UserController::class, 'update']);
     Route::get('/bank', [BankController::class, 'index']);
-
+    Route::apiResource('/acount', OwnerAccountController::class)->except(['index','show']);
 });
 
     //           Auth Route          //
@@ -27,3 +29,5 @@ Route::post('/resendOTP',[OTPController::class,'resendOTP']);
     //             Forget Password     //
 Route::post('/forgetPassword', [ForgetPasswordController::class,'forgetPassword']);
 Route::post('/resetPassword', [ForgetPasswordController::class,'resetPassword']);
+
+Route::get('/EstablishmentType', [EstablishmentTypeController::class, 'index']);
