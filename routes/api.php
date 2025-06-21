566aaf10aed12dc9efdb41ffba05a84c4a7e382a
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-06-21 21:39:39 +0300

    add RegionController , Regionmodel, regionsMigration

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\BankController;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\RegionController;
 use App\Http\Controllers\API\Auth\OtpController;
 use App\Http\Controllers\API\OwnerAccountController;
 use App\Http\Controllers\API\Auth\UserAuthController;
@@ -31,3 +32,7 @@ Route::post('/forgetPassword', [ForgetPasswordController::class,'forgetPassword'
 Route::post('/resetPassword', [ForgetPasswordController::class,'resetPassword']);
 
 Route::get('/EstablishmentType', [EstablishmentTypeController::class, 'index']);
+
+Route::get('/regions/parents', [RegionController::class,'getParents']);
+Route::get('/regions/{id}/children', [RegionController::class,'getChildren']);
+Route::get('/region',[RegionController::class,'index']);
