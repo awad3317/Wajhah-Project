5d071e98a1f51e7e4d8addc2ff943c46a11c45af
awad3317 <awad3317@icloud.com>
2025-06-24 07:29:33 +0300

    Update EstablishmentType.php

--- app/Models/EstablishmentType.php
+++ app/Models/EstablishmentType.php
@@ -19,4 +19,19 @@ class EstablishmentType extends Model
     {
         return $this->hasMany(Establishment::class);
     }
+
+    /**
+     * الحصول على المسار الكامل (URL) للأيقونة.
+     * هذا الـ accessor سيتم استخدامه عندما تطلب 'full_icon_url' من النموذج.
+     * ويمكنك استخدامه في استجابات الـ API.
+     *
+     * @return string|null
+     */
+    public function getFullIconUrlAttribute(): ?string
+    {
+        if ($this->icon) {
+            return Storage::disk('public')->url($this->icon);
+        }
+        return null; 
+    }
 }
