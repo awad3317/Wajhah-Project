f66be9e630c223eb74d3a56835f36e4cae5203e7
awad3317 <awad3317@icloud.com>
2025-06-24 07:00:32 +0300

    Update EstablishmentTypeSeeder.php

--- database/seeders/EstablishmentTypeSeeder.php
+++ database/seeders/EstablishmentTypeSeeder.php
@@ -16,17 +16,17 @@ class EstablishmentTypeSeeder extends Seeder
             [
                 'name' => 'حجز صالات أفراح',
                 'description' => 'نقدم لك تجربة مميزة مع خدمات إستثنائية لضمان نجاح مناسبتك وتقديمها بشكل رائع.',
-                'icon' => 'Establishment-Type-Icons/Wedding-halls.svg',
+                'icon' => 'storage/Establishment-Type-Icons/Wedding-halls.svg',
             ],
             [
                 'name' => 'حجز مسابح',
                 'description' => 'استمتع بتجربة فريدة وخدمات استثنائية تضمن لك أوقاتًا ممتعة ومليئة بالرفاهية، احجز مسبحك بكل سهولة.',
-                'icon' => 'Establishment-Type-Icons/Swimming-pools.svg',
+                'icon' => 'storage/Establishment-Type-Icons/Swimming-pools.svg',
             ],
             [
                 'name' => 'حجز فنادق',
                 'description' => 'استمتع بتجربة حجز فنادق مميزة توفر لك الراحة والرفاهية، مع خدمات عالية الجودة.',
-                'icon' => 'Establishment-Type-Icons/Hotels.svg',
+                'icon' => 'storage/Establishment-Type-Icons/Hotels.svg',
             ],
             
         ];
