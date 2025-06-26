f193f34fc8e1e90d8f67596d23cce706e81a1258
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-06-26 23:21:08 +0300

    Update BankResource.php

--- app/Filament/Resources/BankResource.php
+++ app/Filament/Resources/BankResource.php
@@ -71,11 +71,11 @@ class BankResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
-                    Tables\Actions\DeleteAction::make(),
                 ]),
             ]);
     }
