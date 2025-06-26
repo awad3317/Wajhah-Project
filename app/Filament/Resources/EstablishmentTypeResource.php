e7dd2ea4ece2dbb957fa795861097f2e4e02582c
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-06-26 22:30:47 +0300

    Update EstablishmentTypeResource.php

--- app/Filament/Resources/EstablishmentTypeResource.php
+++ app/Filament/Resources/EstablishmentTypeResource.php
@@ -63,12 +63,14 @@ class EstablishmentTypeResource extends Resource
         ])
         ->columns([
             Stack::make([
+                Tables\Columns\TextColumn::make('')->grow(),
                 Tables\Columns\ImageColumn::make('icon')
                     ->label('')
                     ->disk('public')
                     ->grow(false)
                     ->size(80)
                     ->alignment('center'),
+                Tables\Columns\TextColumn::make('')->grow(), 
                     
                 Tables\Columns\TextColumn::make('name')
                     ->label('')
