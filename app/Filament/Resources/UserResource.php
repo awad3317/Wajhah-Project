5ac947042579c51dbe12d111f4ec1ea12d9e652d
awad3317 <awad3317@icloud.com>
2025-06-30 15:06:03 +0300

    Update UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -134,6 +134,7 @@ class UserResource extends Resource
     {
         return [
             'index' => Pages\ListUsers::route('/'),
+            'view' => Pages\ViewUser::route('/{record}'),
             // 'create' => Pages\CreateUser::route('/create'),
             // 'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
