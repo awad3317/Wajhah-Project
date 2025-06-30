e46785b1e1335cb8aae219fd3af20992888fa7d5
awad3317 <awad3317@icloud.com>
2025-06-30 06:25:31 +0300

    Update 2025_06_21_182324_create_regions_table.php

--- database/migrations/2025_06_21_182324_create_regions_table.php
+++ database/migrations/2025_06_21_182324_create_regions_table.php
@@ -14,10 +14,7 @@ return new class extends Migration
         Schema::create('regions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('parent_id')
-                  ->nullable()
-                  ->constrained('regions')
-                  ->onDelete('cascade');
+            $table->foreignId('parent_id')->nullable()->constrained('regions')->onDelete('cascade');
             $table->timestamps();
         });
     }
