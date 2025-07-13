d803435e4ec95b6fc3f57b046817acc1311b5ca2
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-07-13 17:26:20 +0300

    Update Dashboardcontroller.php

--- app/Http/Controllers/API/Dashboard/Owner/Dashboardcontroller.php
+++ app/Http/Controllers/API/Dashboard/Owner/Dashboardcontroller.php
@@ -19,6 +19,7 @@ class Dashboardcontroller extends Controller
     public function index()
     {
         $ownerId = auth('sanctum')->user()->id; 
+
         $stats = DB::table('establishments')
         ->leftJoin('reviews', 'establishments.id', '=', 'reviews.establishment_id')
         ->where('establishments.owner_id', $ownerId)
@@ -26,10 +27,17 @@ class Dashboardcontroller extends Controller
         DB::raw('COUNT(DISTINCT establishments.id) as establishments_count'),
         DB::raw('AVG(reviews.rating) as average_rating'),
         DB::raw('COUNT(reviews.id) as total_reviews'))->first();
+
+        $totalBookings = DB::table('bookings')
+        ->join('establishments', 'bookings.establishment_id', '=', 'establishments.id')
+        ->where('establishments.owner_id', $ownerId)
+        ->count();
+
         $myEstablishments = Establishment::where('owner_id', $ownerId)->with(['type', 'region.parent'])->get();
         return ApiResponseClass::sendResponse([
             'my_stablishments_count' => $stats->establishments_count,
             'average_rating' => $stats->average_rating,
+            'bookings_count' => $totalBookings,
             'my_stablishments' => $myEstablishments
         ], 'Dashboard data retrieved successfully.');
     }
